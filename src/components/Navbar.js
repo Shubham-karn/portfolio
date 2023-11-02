b3f267727fb253ef0b777ce0a720ad78c82f8140
Shubham <shubhamkarn164@gmail.com>
2023-11-02 17:23:42 +0545

    navbar customized

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,12 @@
 import Link from 'next/link';
 import React, { useState, useEffect } from "react";
+import {
+  AiFillStar,
+  AiOutlineHome,
+  AiOutlineFundProjectionScreen,
+  AiOutlineUser,
+} from "react-icons/ai";
+import { CgGitFork } from "react-icons/cg";
 
 const Navbar = () => {
   const [expand, updateExpanded] = useState(false);
@@ -20,9 +27,33 @@ const Navbar = () => {
 
 
     return (
-      // <nav>
-      <nav className={`${navColour ? "sticky" : "navbar"}`}>
-        Home
+      <nav className={`${navColour ? "sticky" : "navbar"} flex text-gray-300 mx-10 w-full`}>
+        <div className="">
+          Home
+        </div>
+        <div className='flex justify-end'>
+          <Link href="/destination-page" className='p-4 flex item-center'>
+              <AiOutlineHome style={{ marginTop: "5px" }}/>
+              {" "}
+              <span style={{ marginLeft: "10px" }}>Home</span>
+          </Link>
+          <Link href="/destination-page" className='p-4 flex item-center'>
+              <AiOutlineUser style={{ marginTop: "5px" }}/>
+              {" "}
+              <span style={{ marginLeft: "10px" }}>About</span>
+          </Link>
+          <Link href="/destination-page" className='p-4 flex item-center'>
+              <AiOutlineFundProjectionScreen style={{ marginTop: "5px" }}/>
+              {" "}
+              <span style={{ marginLeft: "10px" }}>Projects</span>
+          </Link>
+        </div>
+        <div className='fork-btn'>
+          <button className='fork-btn-inner flex'>
+          <CgGitFork style={{ fontSize: "1.2em" }} />{" "}
+          <AiFillStar style={{ fontSize: "1.1em" }} />
+          </button>
+        </div>
       </nav>
     );
 };
