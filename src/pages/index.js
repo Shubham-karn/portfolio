85c0dc25fe118962bd008a0144febf89ee7d5d70
Shubham <shubhamkarn164@gmail.com>
2023-11-05 15:45:54 +0545

    icon managed

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,12 @@ import Head from 'next/head';
 import NavBar from '../components/Navbar';
 import Type from '../components/Type';
 import Image from 'next/image';
+import {
+  AiFillGithub,
+  AiOutlineTwitter,
+  AiFillInstagram,
+} from "react-icons/ai";
+import { FaLinkedinIn } from "react-icons/fa";
 
 export default function Index() {
     return (
@@ -92,6 +98,64 @@ export default function Index() {
                 height={280}
               />
               </div>
+            </div>
+            <h1 style={{ fontSize: "2.6em" }} className='text-gray-300 pt-[50px] text-center'>
+              FIND <span className="purple"> ME </span> ON
+            </h1>
+            <div className='text-gray-300 pb-[20px] text-center'>
+            <p>
+              Feel free to <span className="purple">connect </span>with me
+            </p>
+            <ul className="home-about-social-links">
+              <li className="social-icons">
+                <a
+                  href=""
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour  home-social-icons"
+                >
+                  <div className='relative p-[7px]'>
+                    <AiFillGithub className='w-[25px] h-[25px]'/>
+                  </div>
+                </a>
+              </li>
+              <li className="social-icons">
+                <a
+                  href=""
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour  home-social-icons"
+                >
+                  <div className='relative p-[7px]'>
+                    <AiOutlineTwitter className='w-[25px] h-[25px]'/>
+                  </div>
+                </a>
+              </li>
+              <li className="social-icons">
+                <a
+                  href=""
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour  home-social-icons"
+                >
+                  <div className='relative p-[7px]'>
+                    <FaLinkedinIn className='w-[25px] h-[25px]'/>
+                  </div>
+                </a>
+              </li>
+              <li className="social-icons">
+                <a
+                  href=""
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour home-social-icons"
+                >
+                  <div className='relative p-[7px]'>
+                    <AiFillInstagram className='w-[25px] h-[25px]'/>
+                  </div>
+                </a>
+              </li>
+            </ul>
             </div>
           </section>
         </>
