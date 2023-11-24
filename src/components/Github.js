0ce7e6851c5f0d927126d93034c4bc1f2afc5991
Shubham <shubhamkarn164@gmail.com>
2023-11-24 15:34:02 +0545

    added scrollbar

--- src/components/Github.js
+++ src/components/Github.js
@@ -3,24 +3,28 @@ import GitHubCalendar from "react-github-calendar";
 
 const Github = () => {
     return (
-        <div className="text-gray-300 flex flex-col justify-center items-center mt-10 overflow-hidden">
+        <>
+        <div className="text-gray-300 flex flex-col justify-center items-center mt-10">
         <div className="flex flex-col justify-center items-center">
             <h1 className="text-3xl font-bold text-center">
                 <span className="purple">Github</span> Contributions</h1>
-            <p className="text-center text-xl mt-3">
+            <p className="text-center text-xl mt-3 mx-3">
             I am a passionate <span className="purple">developer</span> and I love to contribute to <span className="purple">open source
             projects</span>
             </p>
         </div>
-        <div className="mx-auto mt-10 overflow-auto">
+        </div>
+        <div className="text-gray-300 flex flex-col justify-center items-center mt-10 mx-10 overflow-auto">
         <GitHubCalendar
-    username="shubham-karn"
+    username="soumyajit4419"
     blockSize={15}
     blockMargin={5}
     fullYear={true}
+    colorScheme="dark"
+    fontSize={16}
 />
         </div>
-        </div>
+        </>
     );
 }
 
