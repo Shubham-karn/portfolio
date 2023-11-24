6c4d1ad23234a0e6a8822808ee59949ebf1f0fc5
Shubham <shubhamkarn164@gmail.com>
2023-11-24 19:22:35 +0545

    github package added

--- src/components/Github.js
+++ src/components/Github.js
@@ -1,7 +1,12 @@
-import React from "react";
+import React, { useEffect, useState} from "react";
 import GitHubCalendar from "react-github-calendar";
 
 const Github = () => {
+    const [isBrowser, setIsBrowser] = useState(false);
+
+    useEffect(() => {
+        setIsBrowser(typeof window !== 'undefined');
+    }, []);
     return (
         <>
         <div className="text-gray-300 flex flex-col justify-center items-center mt-10">
@@ -15,15 +20,17 @@ const Github = () => {
         </div>
         </div>
         <div className="text-gray-300 flex flex-col justify-center items-center mt-10 mx-10 overflow-auto">
-        <GitHubCalendar
-    username="shubham-karn"
-    blockSize={15}
-    blockMargin={5}
-    fullYear={true}
-    colorScheme="dark"
-    fontSize={16}
-    showWeekdayLabels={true}
-/>
+        {isBrowser && (
+            <GitHubCalendar
+                username="shubham-karn"
+                blockSize={15}
+                blockMargin={5}
+                fullYear={true}
+                colorScheme="dark"
+                fontSize={16}
+                showWeekdayLabels={true}
+            />
+        )}
         </div>
         </>
     );
